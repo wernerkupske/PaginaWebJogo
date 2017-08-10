4f06b79a4a44f7e67908d5faf50656da653045fd
informatica <informatica@munar.blumenau.ifc.edu.br>
2017-08-10 11:09:19 -0300

    terminar o forum.

---
+++ web/bdForum.sql
@@ -0,0 +1,7 @@
+create database forum;
+use forum;
+create table dados_forum{
+	user varchar(15) not null,
+    texto varchar(100) not null
+};
+
---
+++ web/forumDTO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author informatica
+ */
+public class forumDTO {
+    private String mensag;
+
+    public String getMensag() {
+        return mensag;
+    }
+
+    public void setMensag(String mensag) {
+        this.mensag = mensag;
+    }
+
+    public String getNomeUser() {
+        return nomeUser;
+    }
+
+    public void setNomeUser(String nomeUser) {
+        this.nomeUser = nomeUser;
+    }
+    private String nomeUser;
+    
+}
